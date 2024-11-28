1c98ae25868fd5c67812f90c8679d5c915da109e
ouwargui <guikemon2@gmail.com>
2024-11-28 20:35:43 -0300

    Initial Commit

---
+++ schedy/schedyApp.swift
@@ -0,0 +1,17 @@
+//
+//  schedyApp.swift
+//  schedy
+//
+//  Created by Guilherme D'Alessandro on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct schedyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
