467188072500f27a8923940e3609ef1c2eadfff6
ouwargui <guikemon2@gmail.com>
2025-01-25 22:58:32 -0300

    feat: allow user to change update channel on app

--- schedy/Extensions/BundleExtension.swift
+++ schedy/Extensions/BundleExtension.swift
@@ -15,4 +15,12 @@ extension Bundle {
     var buildVersion: String? {
         return infoDictionary?["CFBundleVersion"] as? String
     }
+
+    var schedyCommit: String? {
+        return infoDictionary?["SchedyCommit"] as? String
+    }
+
+    var isBeta: Bool? {
+        return infoDictionary?["Beta"] as? Bool
+    }
 }
